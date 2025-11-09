885322edbfb54ca7fb44224a2e37bd8704b56666
Lam <lamnguyen4791@gmail.com>
2025-11-09 12:43:28 +0700

    RedisConfig

---
+++ src/main/java/com/vnpt/mini_project_java/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package com.vnpt.mini_project_java.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.net.ssl.SSLParameters;
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName("redis-12018.c326.us-east-1-3.ec2.redns.redis-cloud.com");
+        redisConfig.setPort(12018);
+        redisConfig.setPassword("F6ok50nxNzzSiM9Fhl3hjHfBDXPp1gGP");
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .and()
+                .commandTimeout(Duration.ofSeconds(10))
+                .shutdownTimeout(Duration.ofMillis(100))
+                .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
